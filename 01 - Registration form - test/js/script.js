0c4316c6d381d0aae9a116689b20196bd44da781
Jan Košta <jan.kosta96@gmail.com>
2023-04-04 18:25:46 +0200

    Task finished

--- 01 - Registration form - test/js/script.js
+++ 01 - Registration form - test/js/script.js
@@ -1,14 +1,27 @@
 function registerAlert() {
-    var nameInput = document.getElementById('name')
-    var emailInput = document.getElementById('email')
-    alert('User ' + nameInput.value + ' reistred with email ' + emailInput.value)
+    var nameInput = document.getElementById('name-signup')
+    var emailInput = document.getElementById('email-signup')
+    alert('User ' + nameInput.value + ' registred with email ' + emailInput.value)
 }
 
 function backToLogin() {
-    var x = document.getElementById('toggleLogin');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else  {
-        x.style.display = 'none';
+    var x = document.getElementById("hiddenLogin");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
     }
+}
+
+function showLogin() {
+    // První možnost zápisu
+    document.getElementById('login').classList.remove('hidden')
+
+    // Druhá možnost zápisu
+    var register = document.getElementById('register');
+    register.classList.add('hidden')
+}
+function showRegister() {
+    document.getElementById('register').classList.remove('hidden');
+    document.getElementById('login').classList.add('hidden');
 }
--- 01 - Registration form/js/script.js
+++
@@ -1,5 +0,0 @@
-function registerAlert() {
-    var nameInput = document.getElementById('name')
-    var emailInput = document.getElementById('email')
-    alert('User ' + nameInput.value + ' reistred with email ' + emailInput.value)
-}
