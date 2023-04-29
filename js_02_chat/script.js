04796baae4111301f12d29d96a848e8abf632f6e
Jan Košta <jan.kosta96@gmail.com>
2023-04-29 11:11:49 +0200

    New lesson added

--- js_02_chat/script.js
+++ js_02_chat/script.js
@@ -1,2 +1,13 @@
+function createHtmlFromMessage() {
+    const parent = document.getElementById('chat-body')
+    let textMessage = document.getElementById('chat-input')
+    return `<div class="chat__row">
+        <div class="chat__avatar chat__avatar--user1"></div>
+        <div class="chat__message">textMessage</div>
+    </div>
+    `
+}
 
+function renderMessageFromInput() {
 
+}
