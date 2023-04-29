67d2c556d7c0724b20e7c151b8a6afc1be086617
Jan Košta <jan.kosta96@gmail.com>
2023-04-29 14:41:43 +0200

    Chat

--- js_02_chat/script.js
+++ js_02_chat/script.js
@@ -1,13 +1,38 @@
-function createHtmlFromMessage() {
+function createHtmlFromMessage(message, isResponse = false) {
+    // const modifier = isResponse === true ? 'chat__row--response' : ''
+    let modifier = ''
+    if (isResponse === true) {
+        modifier = 'chat__row--response'
+    }
+    return `<div class="chat__row ${modifier}">
+                <div class="chat__avatar"></div>
+                <div class="chat__message">${message.value}</div>
+            </div>`
+}
+
+
+function renderMessageFromInput(inputElement, isResponse = false) {
     const parent = document.getElementById('chat-body')
-    let textMessage = document.getElementById('chat-input')
-    return `<div class="chat__row">
-        <div class="chat__avatar chat__avatar--user1"></div>
-        <div class="chat__message">textMessage</div>
-    </div>
-    `
-}
-
-function renderMessageFromInput() {
+    let htmlMessage = createHtmlFromMessage(inputElement, isResponse)
+    parent.innerHTML += htmlMessage
 
 }
+
+let input = document.getElementById('chat-input');
+input.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter' && input.value.trim() !== '') {
+        event.preventDefault()
+        renderMessageFromInput(input)
+        input.value = ''
+    }
+
+})
+
+
+const button = document.getElementById('responseMessage')
+button.addEventListener('click', function () {
+    renderMessageFromInput(input, true)
+    input.value = ''
+
+})
+
